b767dbe19036b68933e38ada9c4a26d8fe0e7cf6
Rufius <abdurahmanov.r.n@gmail.com>
2025-12-06 18:01:50 +0100

    Some boilerplate code

---
+++ ExternalSorting/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExternalSorting
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Sort("sample.txt");
+        }
+
+        static void Sort(string filePath) {
+            var lines = File.ReadLines(filePath).ToList();
+            
+            lines.Sort();
+
+            lines.ForEach((line) => Console.WriteLine(line));
+        }
+    }
+}
