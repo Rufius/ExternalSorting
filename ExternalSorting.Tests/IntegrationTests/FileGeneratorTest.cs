a4249c15dcd1819bd5a5380dd99c0de03c0d795a
Rufius <abdurahmanov.r.n@gmail.com>
2025-12-11 12:15:01 +0100

    File generator - boiler plate code

---
+++ ExternalSorting.Tests/IntegrationTests/FileGeneratorTest.cs
@@ -0,0 +1,17 @@
+﻿using ExternalSorting.Generator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalSorting.Tests.IntegrationTests {
+    internal class FileGeneratorTest {
+        [Test]
+        public async Task Generate() {
+            var generator = new FileGenerator(99999);
+            await generator.Generate("test.txt", 1024);
+
+        }
+    }
+}
---
+++ ExternalSorting/Generator/FileGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalSorting.Generator {
+    public class FileGenerator {
+        private readonly int NewlineBytesCount;
+        private readonly int MaxNumber;
+
+        Random _random = new Random();
+       
+        public FileGenerator(int maxNumber) {
+            MaxNumber = maxNumber;
+            NewlineBytesCount = Encoding.UTF8.GetByteCount(Environment.NewLine);
+        }
+
+        public async Task Generate(string filePath, int fileSize) {
+            int currentSize = 0;
+
+            using (var streamWriter = new StreamWriter(filePath)) {
+                while (currentSize < fileSize) {
+                    string line = GenerateLine();
+                    await streamWriter.WriteLineAsync(line);
+                    currentSize += Encoding.UTF8.GetByteCount(line) + NewlineBytesCount;
+                }
+            }
+        }
+
+        private string GenerateLine() {
+            return $"{_random.Next(MaxNumber)}. test test test";
+        }
+    }
+}
