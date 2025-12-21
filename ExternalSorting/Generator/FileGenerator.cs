5311c475a41c1d65735faeee06f5280d3d7a4292
Rufius <abdurahmanov.r.n@gmail.com>
2025-12-21 14:17:01 +0100

    File generator - insert duplicate values

--- ExternalSorting/Generator/FileGenerator.cs
+++ ExternalSorting/Generator/FileGenerator.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using System.Collections.Concurrent;
 using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace ExternalSorting.Generator {
     public class FileGenerator {
@@ -16,7 +11,9 @@ namespace ExternalSorting.Generator {
 
         private readonly ILineGenerator _lineGenerator;
         private readonly ILogger _logger;
-       
+
+        private BlockingCollection<string> _lines = new BlockingCollection<string>();
+
         public FileGenerator(int maxNumber, ILineGenerator lineGenerator, ILogger logger) {
             MaxNumber = maxNumber;
             NewlineBytesCount = Encoding.UTF8.GetByteCount(Environment.NewLine);
@@ -25,42 +22,115 @@ namespace ExternalSorting.Generator {
         }
 
         public async Task Generate(string filePath, int fileSize) {
-            int currentSize = 0;
-            using (var streamWriter = new StreamWriter(filePath)) {
-                while (currentSize < fileSize) {
+            Task producerTask = Task.Run(async () => await ProduceLinesAsync(fileSize));
+            Task writerTask = Task.Run(async () => await WriteLinesAsync(filePath));
 
-                    _logger?.Log(LogLevel.Information, "Generating next batch...");
-
-                    // generate a batch of lines
-                    List<string> texLines = new List<string>();
-                    for (int i = 0; i < NumberOfLinesPerBatch; i++) {
-                        _logger?.Log(LogLevel.Information, $"Generating batch line #{i}");
-                        string? textLine = await _lineGenerator.GenerateAsync();
-                        texLines.Add(textLine);
-                    }
-
-                    _logger?.Log(LogLevel.Information, "Batch is generated.");
-
-                    _logger?.Log(LogLevel.Information, "Writing batch lines to the output file...");
-
-                    // write batch lines to the output file
-                    foreach (string texLine in texLines) {
-                        int number = _random.Next(MaxNumber);
-                        string line = $"{number}. {texLine}";
-                        await streamWriter.WriteLineAsync(line);
-                        currentSize += Encoding.UTF8.GetByteCount(line) + NewlineBytesCount;
-                    }
-
-                    _logger?.Log(LogLevel.Information, "Batch is written to the output file");
-                }
-            }
+            await writerTask;
+            await producerTask;
         }
 
-        private async Task<string> GenerateLineAsync() {
-            int number = _random.Next(MaxNumber);
-            string? text = await _lineGenerator.GenerateAsync();
+        private async Task ProduceLinesAsync(int fileSize) {
+            _logger.Log(LogLevel.Information, "Starting generating lines");
 
-            return $"{number}. {text}";
+            int currentSize = 0;
+
+            var duplicatesConfig = new DuplicatesConfig(_random, 4, 100);
+
+            while (currentSize < fileSize) {
+                string text = await GetTextLineAsync(duplicatesConfig); 
+                int number = _random.Next(MaxNumber);
+                string line = $"{number}. {text}";
+
+                _lines.Add(line);
+
+                _logger.Log(LogLevel.Information, $"Line generated: '{line}'");
+
+                currentSize += Encoding.UTF8.GetByteCount(line) + NewlineBytesCount;
+
+                duplicatesConfig.TickIteration();
+            }
+
+            // make sure that file contains at least one duplicate value
+            _lines.Add($"{_random.Next(MaxNumber)}. {duplicatesConfig.GetDuplicateText()}");
+
+            _lines.CompleteAdding();
+            _logger.Log(LogLevel.Information, "Lines generation is complete");
+        }
+
+        private async Task<string> GetTextLineAsync(DuplicatesConfig config) {
+            var text = string.Empty;
+
+            if (config.ShouldInsertDuplicate) {
+                text = config.GetDuplicateText();
+            } else {
+                // generate new text line
+                text = await _lineGenerator.GenerateAsync();
+
+                // if necessary setup insertion of duplicate values 
+                config.SetUpIfNecessary(text);
+            }
+
+            return text;
+        }
+
+        private async Task WriteLinesAsync(string filePath) {
+            _logger.Log(LogLevel.Information, "Starting writing lines");
+
+            using (var streamWriter = new StreamWriter(filePath)) {
+                foreach (var line in _lines.GetConsumingEnumerable()) {
+                    await streamWriter.WriteLineAsync(line);
+                    _logger.Log(LogLevel.Information, $"Line written: '{line}'");
+                }
+            }
+
+            _logger.Log(LogLevel.Information, "Writing lines is complete");
+        }
+
+        private class DuplicatesConfig {
+            private Random _random;
+            private readonly int _maxNumberOfIterationsBeforeDuplicate = 0;
+            private readonly int _maxNumberOfDuplicates = 0;
+            public DuplicatesConfig(Random random, int maxNumberOfDuplicates, int maxNumberOfIterationsBeforeDuplicate) {
+                _random = random;
+                _maxNumberOfDuplicates = maxNumberOfDuplicates;
+                _maxNumberOfIterationsBeforeDuplicate = maxNumberOfIterationsBeforeDuplicate;
+            }
+
+            public string _duplicateText = string.Empty;
+            public int _numberOfIterationsBeforeInsertingDuplicate = 0;
+            public int _numberOfDuplicateValues = 0;
+            public int _duplicateIterationCounter = 0;
+            public int _duplicateValuesCounter = 0;
+            public bool _isInsertingDuplicates = false;
+
+            public bool ShouldInsertDuplicate => _isInsertingDuplicates && (_duplicateIterationCounter == _numberOfIterationsBeforeInsertingDuplicate);
+
+            public void SetUpIfNecessary(string text) {
+                if (_isInsertingDuplicates) return;
+
+                _duplicateText = text;
+                _numberOfDuplicateValues = _maxNumberOfDuplicates; // how many times to insert this text
+                _numberOfIterationsBeforeInsertingDuplicate = _maxNumberOfIterationsBeforeDuplicate; // insert duplicate value after that many lines
+                _isInsertingDuplicates = true;
+            }
+
+            public string GetDuplicateText() {
+                _duplicateValuesCounter--;
+
+                // reset if necessary
+                if (_duplicateValuesCounter == 0) {
+                    _numberOfIterationsBeforeInsertingDuplicate = 0;
+                    _numberOfDuplicateValues = 0;
+                    _duplicateIterationCounter = 0;
+                    _isInsertingDuplicates = false;
+                }
+
+                return _duplicateText;
+            }
+
+            public void TickIteration() {
+                _numberOfIterationsBeforeInsertingDuplicate--;
+            }
         }
     }
 }
--- ExternalSorting/Program.cs
+++ ExternalSorting/Program.cs
@@ -6,27 +6,36 @@ namespace ExternalSorting {
         private static ILogger _logger = new CustomConsoleLogger();
 
         static async Task Main(string[] args) {
+            var externalSorter = new ExternalSorter(Constants.ChunkSizeInBytes);
+            var generator = new FileGenerator(99999, new RandomLineGenerator(), new CustomConsoleLogger());
+
             while (true) {
                 Console.Write("Choose action ([S] Sort, [G] Generate file, [Q] Quit): ");
+
                 var actionKey = Console.ReadKey();
                 Console.WriteLine();
 
                 if (actionKey.KeyChar == 'S' || actionKey.KeyChar == 's') {
                     _logger.Log(LogLevel.Information, "Sorting is starting...");
 
-                    var externalSorter = new ExternalSorter(Constants.ChunkSizeInBytes);
                     await externalSorter.SortAsync("sample.txt");
+
+                    _logger.Log(LogLevel.Information, "Sorting is complete.");
+
                 } else if (actionKey.KeyChar == 'G' || actionKey.KeyChar == 'g') {
                     _logger.Log(LogLevel.Information, "Generation is starting...");
 
-                    var generator = new FileGenerator(99999, new RandomLineGenerator(), new CustomConsoleLogger());
-                    await generator.Generate("test.txt", 1024*1024*1);
+                    await generator.Generate("test.txt", 1024 * 1024 * 500);
 
-                    _logger.Log(LogLevel.Information, "The file was generated.");
+                    _logger.Log(LogLevel.Information, "Generation is complete.");
 
                 } else if (actionKey.KeyChar == 'Q' || actionKey.KeyChar == 'q') {
                     break;
+                } else {
+                    Console.WriteLine("Wrong action. Please choose again");
                 }
+
+                Console.WriteLine();
             }
         }
     }
